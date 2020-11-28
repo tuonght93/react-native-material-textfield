93bc16df9f8758a4db32e800d1fb57a61ebf0a9f
tuonght93 <tuonghttt52@gmail.com>
2020-11-29 03:03:02 +0700

    Update index.js

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -158,7 +158,7 @@ export default class TextField extends PureComponent {
       let toValue = this.focusState(props.error, state.focused);
 
       Animated
-        .timing(focus, { toValue, duration, useNativeDriver: true })
+        .timing(focus, { toValue, duration })
         .start(this.onFocusAnimationEnd);
     }
   }
